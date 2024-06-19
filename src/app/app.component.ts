a0340c67bc83597b0f6f0df2dad61774765eb071
ale94 <alejandrorua111@gmail.com>
2024-06-18 23:58:11 -0300

    Se creo invoice component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InvoiceComponent } from './components/invoice/invoice.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, InvoiceComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'invoice-app';
